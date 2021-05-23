94f09c40d0628d7e07533398e0279b391c3ef8c8
dariopssss <84654740+dariopssss@users.noreply.github.com>
2021-05-23 17:41:47 -0400

    Add files via upload

---
+++ viajes-chile/assets/js/scripts.js
@@ -0,0 +1,20 @@
+$(function(){
+    $(".titulo").click(function(event){
+        $(this).toggle(800);
+
+    });
+
+});
+
+$(function(){
+    $("p").click(function(event){
+        $(this).fadeOut(800);
+
+    });
+
+});
+
+
+function myfunction(){
+   alert("¿está seguro/a que desea abandonar viajes-chile?")
+}
